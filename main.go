f4a0c1840b4603dcf228238a1c15aeeec5221afe
IcyLava <icylavah@gmail.com>
2023-02-08 18:33:51 +0200

    Implement astar path finding, add Rat entity that follows the player

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	// "fmt"
 	"math"
 
 	"github.com/nsf/termbox-go"
@@ -11,6 +13,7 @@ const height = 20
 
 var grid [width][height]TileType
 var entities []Entity
+var playerMoved bool = false
 
 func assert1(err error) {
 	if err != nil {
@@ -25,14 +28,120 @@ func assert2[V any](value V, err error) V {
 	return value
 }
 
+func heuristic(from Position, to Position) int {
+	dx := float64(to.x - from.x)
+	dy := float64(to.y - from.y)
+	result := int(math.Abs(dx) + math.Abs(dy))
+	return result
+}
+
+func distanceToNeighbor(neighbor Position) int {
+	switch grid[neighbor.x][neighbor.y] {
+	case Empty:
+		return 1
+	case Wall:
+		return 1000
+	default:
+		return 1
+	}
+}
+
+func getScoreOrLarge(posMap map[Position]int, pos Position) int {
+	score, ok := posMap[pos]
+	if !ok {
+		score = 1000000
+	}
+	return score
+}
+
+func findPath(start Position, end Position) ([]Position, error) {
+	openSet := []Position{start}
+	
+	cameFrom := make(map[Position]*Position)
+	gScore := make(map[Position]int)
+	gScore[start] = 0
+	
+	fScore := make(map[Position]int)
+	fScore[start] = heuristic(start, end)
+	
+	for len(openSet) != 0 {
+		currentI := 0
+		current := openSet[currentI]
+		for i := 1; i < len(openSet); i++ {
+			node := openSet[i]
+			currentScore := getScoreOrLarge(fScore, current)
+			nodeScore := getScoreOrLarge(fScore, node)
+			if nodeScore < currentScore {
+				current = node
+				currentI = i
+			}
+		}
+		if current == end {
+			totalPath := []Position{current}
+			for {
+				next := cameFrom[current]
+				if next == nil {break}
+				current = *next
+				totalPath = append(totalPath, current)
+			}
+			for i, j := 0, len(totalPath)-1; i < j; i, j = i+1, j-1 {
+				totalPath[i], totalPath[j] = totalPath[j], totalPath[i]
+			}
+			return totalPath, nil
+		}
+		openSet[currentI] = openSet[len(openSet)-1]
+		openSet = openSet[:len(openSet)-1]
+		for dy := -1; dy <= 1; dy++ {
+			for dx := -1; dx <= 1; dx++ {
+				if dx == 0 && dy == 0 {continue}
+				
+				pos := Position{
+					x: current.x + dx,
+					y: current.y + dy,
+				}
+				if pos.x < 0 || pos.y < 0 || pos.x >= width || pos.y >= height {
+					continue
+				}
+				
+				if grid[pos.x][pos.y] == Wall {continue}
+				
+				neighbor := pos
+				tentativeGScore := getScoreOrLarge(gScore, current) + distanceToNeighbor(neighbor)
+				neighborScore := getScoreOrLarge(gScore, neighbor)
+				if tentativeGScore < neighborScore {
+					from := current
+					cameFrom[neighbor] = &from
+					gScore[neighbor] = tentativeGScore
+					fScore[neighbor] = tentativeGScore + heuristic(neighbor, end)
+					var found bool
+					for _, node := range openSet {
+						if node == neighbor {
+							found = true
+							break
+						}
+					}
+					if !found {
+						openSet = append(openSet, neighbor)
+					}
+				}
+			}
+		}
+	}
+	return nil, errors.New("could not find path")
+}
+
 type Entity interface {
 	GetSymbol() rune
 	
 	GetPosition() (int, int)
 	SetPosition(x int, y int)
 	
+	GetTargetPosition() Position
 	Move(dx int, dy int)
-	TakesInput() bool
+}
+
+type Controlled interface {
+	SetLastInput(dx int, dy int)
 }
 
 type Position struct {
@@ -47,13 +156,65 @@ func (pos *Position) SetPosition(x int, y int) {
 	pos.x, pos.y = x, y
 }
 
-type Player struct {
+type Rat struct {
 	Position
 }
 
-func (entity *Player) GetSymbol() rune {
+func (entity Rat) GetSymbol() rune {
+	return 'r'
+}
+
+func (entity Rat) GetTargetPosition() Position {
+	var nearestPlayer Entity
+	for _, entity := range entities {
+		switch entity.(type) {
+		case *Player:
+			// TODO: find nearest player
+			nearestPlayer = entity
+		}
+	}
+	if nearestPlayer != nil {
+		x, y := nearestPlayer.GetPosition()
+		return Position{x, y}
+	}
+	x, y := entity.GetPosition()
+	return Position{x, y}
+}
+
+// TODO: remove code duplication
+func (entity *Rat) Move(dx int, dy int) {
+	if math.Abs(float64(dx)) > 1 || math.Abs(float64(dy)) > 1 {
+		panic("moving further than 1 tile")
+	}
+	tile := grid[entity.x + dx][entity.y + dy]
+	if tile != Wall {
+		entity.x += dx
+		entity.y += dy
+	}
+}
+
+type Player struct {
+	Position
+	lastInputDX int
+	lastInputDY int
+}
+
+func (entity Player) GetSymbol() rune {
 	return '@'
 }
+
+func (entity Player) GetTargetPosition() Position {
+	return Position{
+		x: entity.x + entity.lastInputDX,
+		y: entity.y + entity.lastInputDY,
+	}
+}
+
+func (entity *Player) SetLastInput(dx int, dy int) {
+	entity.lastInputDX = dx
+	entity.lastInputDY = dy
+}
+
 func (entity *Player) Move(dx int, dy int) {
 	if math.Abs(float64(dx)) > 1 || math.Abs(float64(dy)) > 1 {
 		panic("moving further than 1 tile")
@@ -64,6 +225,7 @@ func (entity *Player) Move(dx int, dy int) {
 		entity.y += dy
 	}
 }
+
 func (entity *Player) TakesInput() bool {
 	return true
 }
@@ -81,7 +243,8 @@ func redrawGrid() {
 			case Empty:
 				termbox.SetChar(x, y, '.')
 			case Wall:
-				termbox.SetChar(x, y, '#')
+				termbox.SetChar(x, y, '▓')
+				// termbox.SetCell(x, y, '▓', termbox.ColorBlue, termbox.ColorDefault)
 			}
 		}
 	}
@@ -91,8 +254,6 @@ func redrawGrid() {
 		termbox.SetChar(x, y, symbol)
 	}
 }
-
-func noop(value any) {}
 
 func main() {
 	assert1(termbox.Init())
@@ -110,9 +271,18 @@ func main() {
 	}
 	
 	entities = append(entities, &Player{
-		Position{
+		Position: Position{
 			x: 20,
 			y: 10,
+		},
+		lastInputDX: 0,
+		lastInputDY: 0,
+	})
+	
+	entities = append(entities, &Rat{
+		Position: Position{
+			x: 30,
+			y: 12,
 		},
 	})
 
@@ -146,10 +316,35 @@ game:
 			}
 			if dx != 0 || dy != 0 {
 				for _, entity := range entities {
-					if entity.TakesInput() {
-						entity.Move(dx, dy)
+					controlled, ok := entity.(Controlled)
+					if ok {
+						controlled.SetLastInput(dx, dy)
+						// NOTE: might not be a player though
+						playerMoved = true
 					}
 				}
+			}
+			if playerMoved {
+				playerMoved = false
+				
+				targetMap := make(map[Entity]Position)
+				for _, entity := range entities {
+					targetMap[entity] = entity.GetTargetPosition()
+				}
+				
+				for entity, tpos := range targetMap {
+					ex, ey := entity.GetPosition()
+					epos := Position{ex, ey}
+					// fmt.Printf("%v %v\n", epos, tpos)
+					if tpos != epos {
+						path, err := findPath(epos, tpos)
+						if err == nil {
+							nextPos := path[1]
+							entity.Move(nextPos.x - epos.x, nextPos.y - epos.y)
+						}
+					}
+				}
+				
 				redrawGrid()
 				termbox.Flush()
 			}
